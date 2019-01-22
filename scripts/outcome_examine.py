ce976dba797cac6d44adfb9c42a0a37f736a81dd
Theodore Morley <ted@elmgatefarm.com>
2019-01-22 17:01:43 -0600

    Examining microarray variable relationships

---
+++ scripts/outcome_examine.py
@@ -0,0 +1,16 @@
+import sys
+import pandas as pd
+import matplotlib
+matplotlib.use('agg')
+import matplotlib.pyplot as plt
+
+'''
+input_var: Single string column name in df.
+outcome_vars: List of string column names to test and output the relationship of with input_var (also must be in df)
+df: A dataframe containing columns corresponding to the above variables.
+'''
+def test_relationships(input_var, outcome_vars, df):
+    #
+
+if __name__=='__main__':
+    #
