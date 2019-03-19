3d239aa5a200b7d29d747e17032ff3cd1bcadafd
Theodore Morley <ted@elmgatefarm.com>
2019-03-19 17:05:40 -0500

    Generalized functions

--- scripts/CMA_Analysis.py
+++ scripts/CMA_Analysis.py
@@ -1,6 +1,8 @@
 import pandas as pd
 import numpy as np
 import sys
+import umap
+from sklearn.decomposition import PCA
 import statsmodels.formula.api as smf
 import statsmodels.genmod.families.family as fam
 import matplotlib
@@ -9,6 +11,10 @@ import matplotlib.pyplot as plt
 import seaborn as sns
 import datetime
 from scipy.stats import chi2_contingency as chi2_c
+from assocplots.manhattan import *
+import datashader as ds
+import datashader.utils as utils
+import datashader.transfer_functions as tf
 
 def dconvert(d):
     return datetime.datetime.strptime(d,"%Y-%m-%d")
@@ -44,6 +50,133 @@ def get_phe_counts(df, pl, name):
     sum_df = phe_sums.reset_index()
     sum_df.columns=['PHECODE', name]
     return sum_df
+
+
+
+def CNV_colored_plots_smallset(split, mets):
+    df = pd.read_csv(sys.argv[1], dtype={'phecode':str, 'location':str, 'Result':str})
+    phecodes = pd.read_csv(sys.argv[2], dtype=str)
+    phe_list = [phe for phe in list(phecodes.PHECODE.unique()) if phe in df]
+    phedf = df.loc[:, phe_list]
+    phedf[phedf>0] = 1
+    df[phe_list] = phedf
+    print(df[phe_list].head())
+    top_locations = None
+    #if only_gln:
+    #    #Only consider gains, losses, and normal results
+    if split:
+        #Consider gains and losses within an abnormal result seperately
+        top_locations = df.loc[df.Result.isin(['Gain','Loss'])].groupby(['Result','location']).count().GRID.sort_values(ascending=False).reset_index()
+    #Umap visualization
+    locs = [top_locations.iloc[l].location for l in range(10)]
+    res = [top_locations.iloc[l].Result for l in range(10)]
+    colors = [ '#9e0142'
+            '#d8434e'
+            '#f67a49'
+            '#fdbf6f'
+            '#feeda1'
+            '#f1f9a9'
+            '#bfe5a0'
+            '#74c7a5'
+            '#378ebb'
+            '#5e4fa2']
+    #Put together all information
+    pal = list(zip(locs,res,colors))
+
+
+    classes = [l+'_'+r for l,r in zip(locs,res)]
+    
+
+    df['concat'] = df.location+'_'+df.Result
+    df = df.loc[df['concat'].isin(classes)]
+    df=df.reset_index()
+    print('beginning umap')
+    for met in mets:
+        print(met)
+        ump = umap.UMAP(metric=met)
+        embedding = ump.fit_transform(df[phe_list])
+        viz_df = pd.DataFrame(embedding, columns=('x', 'y'))
+        viz_df['class']=pd.Series(df['concat'], dtype='category')
+
+        color_key = {l+r:c for l,r,c in pal}
+
+        sns.pairplot(x_vars=['x'], y_vars=['y'], data=viz_df, hue="class", size=6)
+        plt.title('Locations in UMAP: Small set')
+        plt.savefig(sys.argv[3]+'_'+met+'.png')
+        plt.clf()
+
+        #calculate centroids
+        centroid_df = viz_df.groupby('class').mean().reset_index()
+        sns.pairplot(x_vars=['x'], y_vars=['y'], data=centroid_df, hue='class', size=6)
+        plt.title('Centroids of UMAP embedded CNVs: Small Set')
+        plt.savefig(sys.argv[3]+'_'+met+'_centroids.png')
+        plt.clf()
+
+
+def CNV_plots_all(split, mets):
+    #Take the 
+
+'''
+Uses phecodes in dataframe to create principal components/UMAP and see if they cluster well
+'''
+def CNV_colored_plots():
+    df = pd.read_csv(sys.argv[1], dtype={'phecode':str, 'location':str})
+    phecodes = pd.read_csv(sys.argv[2], dtype=str)
+    phe_list = [phe for phe in list(phecodes.PHECODE.unique()) if phe in df]
+    #Umap visualization
+    print('beginning umap')
+    ump = umap.UMAP()
+    embedding = ump.fit_transform(df[phe_list])
+    viz_df = pd.DataFrame(embedding, columns=('x', 'y'))
+    viz_df['class']=df.location#pd.Series(df['location'].values, dtype='category')
+
+    pal = [('2q13', '#9e0142'),
+            ('22q11.21', '#d8434e'),
+            ('16p11.2', '#f67a49'),
+            ('15q11.2', '#fdbf6f'),
+            ('17p12', '#feeda1'),
+            ('X chromosome', '#f1f9a9'),
+            ('7q11.23', '#bfe5a0'),
+            ('15q11.2q13.1', '#74c7a5'),
+            ('1q21.1', '#378ebb'),
+            ('chromosome 21', '#5e4fa2')]
+
+    color_key = {d:c for d,c in pal}
+
+    #Restrict df to specific most common cnvs (top 10)
+    viz_df=viz_df.loc[viz_df['class'].isin(list(zip(*pal))[0])]
+    print(viz_df.head())
+    viz_df['class']=pd.Series(viz_df['class'], dtype='category')
+    print(viz_df.head())
+
+    print('Selected subset of top 10 most common cnvs')
+
+    #Creating image
+    #cvs = ds.Canvas(plot_width=400, plot_height=400)
+    #agg = cvs.points(viz_df, 'x', 'y', ds.count_cat('class'))
+    #img = tf.shade(agg, color_key=color_key, how='eq_hist')
+    
+    #utils.export_image(img, filename='cnv_shade_umap', background='black')
+
+    #image = plt.imread('cnv_shade_umap.png')
+    #fig, ax = plt.subplots(figsize=(6, 6))
+    sns.pairplot(x_vars=['x'], y_vars=['y'], data=viz_df, hue="class", size=6)
+    #plt.setp(ax,xticks=[],yticks=[])
+    plt.title('Locations in UMAP')
+    plt.savefig(sys.argv[3])
+    plt.clf()
+
+
+def plot_phewas_manhattan():
+    df = pd.read_csv(sys.argv[1], dtype={'phecode':str})
+    phewas_category = pd.read_table(sys.argv[2], dtype=str)
+    phewas_category = phewas_category.rename(columns={'phewas_code':'phecode'}).loc[:,["phecode","category"]].drop_duplicates()
+    merged = df.merge(phewas_category, how='left', on='phecode')
+    merged = merged.loc[merged.p>0,:]
+    manhattan(merged.p, merged.phecode.astype(float), merged.category, '', xlabel="category")
+    plt.title("Case vs Control: Chi-Squared")
+    plt.savefig(sys.argv[3])
+    plt.clf()
 
 
 def phewas_upper():
@@ -275,4 +408,6 @@ if __name__=="__main__":
     #ethn_table()
     #sex_table()
     #phecode_table()
-    phewas_upper()
+    #phewas_upper()
+    #plot_phewas_manhattan()
+    CNV_colored_plots_smallset(True, ['jaccard', 'cosine', 'hamming', 'dice', 'correlation', 'russellrao', 'euclidean', 'yule'])
