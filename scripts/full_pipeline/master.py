aeb8350cb4be6d3d08d61153bf2434c72809ece5
Theodore Morley <ted@elmgatefarm.com>
2019-09-03 13:40:57 -0500

    Finished version of master script with included sample data for testing (Does not work with dimensionality reduction due to minimum size reqs)

--- scripts/full_pipeline/master.py
+++ scripts/full_pipeline/master.py
@@ -33,19 +33,23 @@ def main():
     cma_df = pd.read_csv(sys.argv[4])
     fv_out = sys.argv[5]
     param_out = sys.argv[6]
+    print('Loaded args')
     #Set cc_status tag in demo_df
     demo_df['CC_STATUS']=0
     demo_df.loc[demo_df.GRID.isin(cma_df.GRID),'CC_STATUS']=1
     #Match controls #fullpop cases
-    case_control_df = match_controls.cc_match(demo_df, demo_df.loc[demo_df.cc_status==1,:])
+    case_control_df = match_controls.cc_match(demo_df, demo_df.loc[demo_df.CC_STATUS==1,:])
     cc_grids=list(case_control_df.GRID)
+    print('matched controls')
     #Create long df for everyone
     phecodes.columns=['GRID', 'CODE']
     phecodes_group = phecodes.groupby(['GRID','CODE']).size().unstack().fillna(0).astype(int).reset_index()
     long_df = demo_df.merge(phecodes_group, on="GRID", how="outer")
-    long_df[phecodes.CODE.unique()] = df[phecodes.CODE.unique()].fillna(0).astype(int)
+    long_df[phecodes.CODE.unique()] = long_df[phecodes.CODE.unique()].fillna(0).astype(int)
+    print('created long df')
     #Get the weight sum
     weights = pd.Series(weight_df.WEIGHT.values,index=weight_df.PHECODE.astype(str)).to_dict()
+    print('loaded weights')
     #drop cog anoms
     for code in ['758', '758.1', '759', '759.1']:
         if code in long_df:
@@ -54,19 +58,24 @@ def main():
     anc_child_dict = create_weight_df.create_ancestry(phe_list)
     long_df = create_weight_df.leaf_select_codes(long_df, anc_child_dict, phe_list)
     summed_df = create_weight_df.get_sums(long_df, weights)
+    print('summed weights; performed ancestry analysis w/codes')
     #Run sklearn pipeline for grid search
     phe_list = [x for x in phecodes.CODE if x in summed_df.columns]
     results_df, best_estimator = cross_validation_pipeline_probabilistic.sklearn_pipeline(summed_df[phe_list+['weight_sum']], summed_df['CC_STATUS'].astype(int))
     results_df.to_csv(param_out,index=False)
+    print('pipeline complete')
     #retrain best parameters on entire cc set
     best_estimator.fit(summed_df.loc[summed_df.GRID.isin(cc_grids),phe_list+['weight_sum']], summed_df.loc[summed_df.GRID.isin(cc_grids),'CC_STATUS'].astype(int))
+    print('estimator retrained')
     #predict on frequent visitors set
     fv_df=summed_df.loc[summed_df['fv_status']==1].copy()
     fv_df = fv_df.sample(frac=1)
     probs=best_estimator.predict_proba(fv_df[phe_list+['weight_sum']])
+    print('prediction complete')
     #fv_df['control_prob'] = probs[:, 0]
     fv_df['case_prob'] = probs[:, 1]
     fv_df[['GRID','case_prob']].to_csv(fv_out,index=False)
+    print('wrote results')
 
 if __name__=='__main__':
     main()
