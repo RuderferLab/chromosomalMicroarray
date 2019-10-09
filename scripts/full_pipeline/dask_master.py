7a317a9e669dcb681ed6944313dd06189c1ade91
Theodore Morley <ted@elmgatefarm.com>
2019-10-09 15:47:13 -0500

    Fixed version of selecting columns that caused memory error

--- scripts/full_pipeline/dask_master.py
+++ scripts/full_pipeline/dask_master.py
@@ -3,6 +3,7 @@ import gc
 import pandas as pd
 import dask.dataframe as dd
 import match_controls
+import time
 import cross_validation_pipeline_probabilistic
 import create_weight_df
 
@@ -30,34 +31,39 @@ CENSOR=True
 
 #To censor codes, needs GRID, CODE, and DATE columns
 def censor_codes(code_df, cma_df):
+    print(code_df.head())
     #Look at only cma codes
     cma_codes = code_df.loc[code_df.GRID.isin(cma_df.GRID)]
+    print(cma_codes.head())
     cma_merge = cma_codes.merge(cma_df[['GRID','ENTRY_DATE']], how='left', on='GRID')
     cma_merge.ENTRY_DATE = pd.to_datetime(cma_merge.ENTRY_DATE)
     cma_merge.DATE = pd.to_datetime(cma_merge.DATE)
+    print(cma_merge.head())
     #Identify indices to drop
     drop_inds = cma_merge.loc[cma_merge.ENTRY_DATE<=cma_merge.DATE].index
     #Drop them
-    return code_df.drop(drop_inds)
+    return code_df.drop(index=drop_inds)
 
 
 #python master.py demographic_data phecodes weights CMA fv_out param_out
 def main():
+    start_time = time.time()
     #Load in all inputs
     #Demographic data for entire SD
-    demo_df = dd.read_csv(sys.argv[1], sep='|')
+    demo_df = pd.read_csv(sys.argv[1], sep='|')
     #Phecodes for entire SD
     phecodes = dd.read_csv(sys.argv[2], dtype=str)
     #Inverse prevalence weights (across frequent visitors) for all phecodes
     weight_df = pd.read_csv(sys.argv[3], dtype={'PHECODE': str})
     #Chromosomal microarray set (individuals who received a genetic test)
-    cma_df = pd.read_csv(sys.argv[4])
+    cma_df = pd.read_csv(sys.argv[4]).drop_duplicates(subset='GRID')
     #Location (string) where the frequent visitors case probability predictions should be written to
     fv_out = sys.argv[5]
     #Location (string) where the parameters and cross validation results for all tested models should be written to
     param_out = sys.argv[6]
     cpu_num = int(sys.argv[7])
     print('Loaded args')
+    print("--- %s seconds ---" % (time.time() - start_time))
     #Set cc_status (case control status) tag in demo_df to indicate which patients received a CMA
     ##demo_df['CC_STATUS']=0
     ##demo_df.loc[demo_df.GRID.isin(cma_df.GRID),'CC_STATUS']=1
@@ -65,26 +71,34 @@ def main():
     print('Beginning to match controls')
     cc_grids = match_controls.dask_cc_match(demo_df, cma_df.GRID.unique(), 4)
     print('matched controls')
+    print("--- %s seconds ---" % (time.time() - start_time))
     #Create long df for case control set
     phecodes.columns=['PERSON_ID','GRID', 'CODE','DATE']
     cc_phecodes = phecodes.loc[phecodes.GRID.isin(cc_grids)].compute().reset_index(drop=True)
+    print('selected cc_phecodes')
+    print("--- %s seconds ---" % (time.time() - start_time))
     if CENSOR:
         cc_phecodes = censor_codes(cc_phecodes, cma_df)
+        print('censored cc codes')
+        print("--- %s seconds ---" % (time.time() - start_time))
     cc_phecodes_group = cc_phecodes.groupby(['GRID','CODE']).size().unstack().fillna(0).astype(int).reset_index()
-    long_cc_df = demo_df.loc[demo_df.GRID.isin(cc_grids)].compute().merge(cc_phecodes_group, on="GRID", how="left")
+    long_cc_df = demo_df.loc[demo_df.GRID.isin(cc_grids)].copy().merge(cc_phecodes_group, on="GRID", how="left")
     long_cc_df[cc_phecodes.CODE.unique()] = long_cc_df[cc_phecodes.CODE.unique()].fillna(0).astype(int)
     long_cc_df['CC_STATUS']=0
     long_cc_df.loc[long_cc_df.GRID.isin(cma_df.GRID),'CC_STATUS']=1
     print('Created long cc df')
+    print("--- %s seconds ---" % (time.time() - start_time))
     #Create long df for fv
     fv_phecodes = phecodes.loc[phecodes.GRID.isin(demo_df.loc[demo_df.FV_STATUS==1,'GRID'])].compute().reset_index(drop=True)
     fv_phecodes_group = fv_phecodes.groupby(['GRID','CODE']).size().unstack().fillna(0).astype(int).reset_index()
-    long_fv_df = demo_df.loc[demo_df.FV_STATUS==1].compute().merge(fv_phecodes_group, on="GRID", how="left")
+    long_fv_df = demo_df.loc[demo_df.FV_STATUS==1].copy().merge(fv_phecodes_group, on="GRID", how="left")
     long_fv_df[fv_phecodes.CODE.unique()] = long_fv_df[fv_phecodes.CODE.unique()].fillna(0).astype(int)
     print('created long fv df')
+    print("--- %s seconds ---" % (time.time() - start_time))
     ##Get the weight sum (Phenotypic risk score)
     weights = pd.Series(weight_df.WEIGHT.values,index=weight_df.PHECODE.astype(str)).to_dict()
     print('loaded weights')
+    print("--- %s seconds ---" % (time.time() - start_time))
     #drop cogenital anomalies (would be cheating to use them)
     for code in ['758', '758.1', '759', '759.1']:
         if code in long_cc_df:
@@ -101,26 +115,31 @@ def main():
     long_cc_df['weight_sum']=summed_cc_weight
     long_fv_df['weight_sum']=summed_fv_weight
     print('summed weights; performed ancestry analysis w/codes')
+    print("--- %s seconds ---" % (time.time() - start_time))
     #Run sklearn pipeline for grid search
-    phe_list = [x for x in phecodes.CODE if x in long_cc_df.columns]
+    phe_list = [x for x in list(weight_df.PHECODE.unique()) if x in long_cc_df.columns]
     phe_list.append('weight_sum')
     #Garbage collect time?
     gc.collect()
     results_df, best_estimator = cross_validation_pipeline_probabilistic.sklearn_pipeline(long_cc_df[phe_list], long_cc_df['CC_STATUS'].astype(int), cpu_num)
     results_df.to_csv(param_out,index=False)
     print('pipeline complete')
+    print("--- %s seconds ---" % (time.time() - start_time))
     ##retrain best parameters on entire cc set (new problem, same model)
     best_estimator.fit(long_cc_df[phe_list], long_cc_df['CC_STATUS'].astype(int))
     print('estimator retrained')
+    print("--- %s seconds ---" % (time.time() - start_time))
     #predict on frequent visitors set (minus the cases and controls)
     fv_df=long_fv_df.loc[~long_fv_df.GRID.isin(cc_grids)].copy()
     fv_df = fv_df.sample(frac=1)
     probs=best_estimator.predict_proba(fv_df[phe_list])
     print('prediction complete')
+    print("--- %s seconds ---" % (time.time() - start_time))
     #fv_df['control_prob'] = probs[:, 0]
     fv_df['case_prob'] = probs[:, 1]
     fv_df[['GRID','case_prob']].to_csv(fv_out,index=False)
     print('wrote results')
+    print("--- %s seconds ---" % (time.time() - start_time))
 
 if __name__=='__main__':
     main()
